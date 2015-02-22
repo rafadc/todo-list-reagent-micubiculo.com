4633266b726ef34d10b49af0e03b06300c243c12
Rafa de Castro <rafael@micubiculo.com>
2015-02-22 23:01:38 +0100

    Now we show some tasks in the list

--- src/cljs/todo_list/views/main.cljs
+++ src/cljs/todo_list/views/main.cljs
@@ -1,5 +1,20 @@
 (ns todo-list.views.main)
 
+(defonce tasks (atom []))
+
+(defn- add-task [text]
+  (swap! tasks #(conj % {:text text, :status :notdone})))
+
+(add-task "One task")
+(add-task "Other task")
+
+(defn- show-task [task]
+  "Show a single task"
+  [:div {:class (str "task task-" (name (:status task)))}
+   [:b (:text task)]])
+
 (defn todo-list []
-  [:div [:h2 "TODO list"]
+  [:div
+   [:h2 "TODO list"]
+   (map show-task @tasks)
    [:div [:a {:href "#/about"} "go to about page"]]])
