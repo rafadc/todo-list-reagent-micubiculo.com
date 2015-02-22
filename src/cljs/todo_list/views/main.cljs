9aad56fd9e87b2a26404bdb8161a5b7535274aa8
Rafa de Castro <rafael@micubiculo.com>
2015-02-22 23:32:53 +0100

    Of course it makes no sense to create sample data there

--- src/cljs/todo_list/views/main.cljs
+++ src/cljs/todo_list/views/main.cljs
@@ -5,9 +5,6 @@
 (defn- add-task [text]
   (swap! tasks #(conj % {:text text, :status :notdone})))
 
-(add-task "One task")
-(add-task "Other task")
-
 (defn- show-task [task]
   "Show a single task"
   [:div {:class (str "task task-" (name (:status task)))}
